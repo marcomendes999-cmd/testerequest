afc7f028a2f81db41c7c7cc7adeb0ae00ea587c9
marli29 <marco.mendes@marli.pt>
2026-01-23 18:41:50 +0000

    teste mm

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,7 +14,9 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        
+
+        <link rel="stylesheet" href="/build/assets/app-BU5TSkbz.css">
     </head>
 
     <body class="font-sans text-gray-900 antialiased">
@@ -33,3 +35,5 @@
 
     </body>
 </html>
+
+<script type="module" src="/build/assets/app-DLYOw6CL.js"></script> 
